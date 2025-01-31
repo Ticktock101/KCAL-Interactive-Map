3038d8d348707572118ecea73e6c5ed074de3cb4
Omkar Rayudu <omkarrayudu@gmail.com>
2025-01-31 12:19:20 -0600

    zooming sort of works and added a room number

--- app/component/floor.jsx
+++ app/component/floor.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { View, Animated } from 'react-native';
 import { PinchGestureHandler, State } from 'react-native-gesture-handler';
 import Floor1Svg from "../images/floor1.svg"; // Ensure this is a valid React component
+import { transform } from '@babel/core';
 
 export default function Floor() {
 
@@ -14,35 +15,49 @@ export default function Floor() {
 //     }
 //   };
 
-const scale = useRef(new Animated.Value(1)).current;
 
-  const onPinchGestureEvent = Animated.event(
-    [{ nativeEvent: { scale: scale } }],
-    { useNativeDriver: false }
-  );
 
-  const onPinchHandlerStateChange = ({ nativeEvent }) => {
-    if (nativeEvent.state === State.END || nativeEvent.state === State.CANCELLED) {
-      Animated.spring(scale, {
-        toValue: 1,
-        useNativeDriver: false,
-      }).start();
-    }
-  };
+
+// const scale = useRef(new Animated.Value(1)).current;
+
+  // const onPinchGestureEvent = Animated.event(
+  //   [{ nativeEvent: { scale: scale } }],
+  //   { useNativeDriver: false }
+  // );
+
+  // const onPinchHandlerStateChange = ({ nativeEvent }) => {
+  //   if (nativeEvent.state === State.END || nativeEvent.state === State.CANCELLED) {
+  //     Animated.spring(scale, {
+  //       toValue: 1,
+  //       useNativeDriver: false,
+  //     }).start();
+  //   }
+  // };
+
+
+  // const [scale, setScale] = useState(1);
+
+  // const onPinchEvent = (event) => {
+  //   setScale(event.nativeEvent.scale);
+  // };
+
+  const scale = React.useRef(new Animated.Value(0)).current;
+  const onPinchEvent = Animated.event([{nativeEvent: {scale: scale}}]);
 
   return (
 
     <PinchGestureHandler
-      onGestureEvent={onPinchGestureEvent}
-      onHandlerStateChange={onPinchHandlerStateChange}
+      onGestureEvent={onPinchEvent}
+      // onHandlerStateChange={onPinchHandlerStateChange}
     >
       <View>
         <Animated.View
-          style={{
-            transform: [{ scale: scale }],
-          }}
+          style={{transform: [{ scale }]}}
         >
-          <Floor1Svg width="100%" height="100%" />
+          <Floor1Svg 
+            width="100%" 
+            height="100%" 
+          />
         </Animated.View>
       </View>
     </PinchGestureHandler>
--- metro.config.js
+++ metro.config.js
@@ -33,20 +33,25 @@
 
 //stackoverflow code:
 const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require("nativewind/metro")
 
 const config = getDefaultConfig(__dirname);
 
-const { transformer, resolver } = config;
+// const { transformer, resolver } = config;
 
-config.transformer = {
-  ...transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+// config.transformer = {
+//   ...transformer,
+//   babelTransformerPath: require.resolve('react-native-svg-transformer'),
+// };
+
+// config.resolver = {
+//   ...resolver,
+//   assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+//   sourceExts: [...resolver.sourceExts, 'svg'],
+// };
+
+config.resolver.extraNodeModules = {
+  fs: require.resolve('./mockfs'),
 };
 
-config.resolver = {
-  ...resolver,
-  assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...resolver.sourceExts, 'svg'],
-};
-
-module.exports = config;
+module.exports = withNativeWind(config, { input: "./global.css" });
