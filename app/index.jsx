640d649d4074b29c937559142cf99a7c5cad620a
Omkar Rayudu <omkarrayudu@gmail.com>
2025-02-19 13:37:55 -0600

    conditional rendering with dropdown menu and floors

--- app/index.jsx
+++ app/index.jsx
@@ -14,32 +14,33 @@ export default function Index() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    {label: 'Floor1', value: 'Floor 1'},
-    {label: 'Floor2', value: 'Floor 2'}
+    {label: 'Floor 1', value: 'Floor 1'},
+    {label: 'Floor 2', value: 'Floor 2'}
   ]);
 
 
   return (
     <GestureHandlerRootView className="bg-white">
       <SafeAreaView className="h-screen max-w-full">
-        <View className="flex flex-row  h-auto w-auto">
-          <TouchableOpacity>
+
+        <View className="flex flex-row w-auto justify-between m-4">
+          <View className="">
+            <Image source={require('./images/favicon.png')} className=" "/>
+          </View>
+          
+          <View className=" flex flex-row justify-around w-1/2">
+            <TouchableOpacity>
+              <Image source={require('./images/favicon.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image source={require('./images/favicon.png')}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
             <Image source={require('./images/favicon.png')}/>
-          </TouchableOpacity>
-          <View className="flex-row">
-            <TouchableOpacity>
-              <Image source={require('./images/favicon.png')}/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Image source={require('./images/favicon.png')}/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Image source={require('./images/favicon.png')}/>
             </TouchableOpacity>
           </View>
         </View>
-        <View className="h-auto items-center z-10">
-            {/* <Header /> */}
+        <View className="h-auto items-center z-10 mx-4">
             <DropDownPicker
               open={open}
               value={value}
@@ -47,18 +48,18 @@ export default function Index() {
               setOpen={setOpen}
               setValue={setValue}
               setItems={setItems}
+              placeholder="Floor 1"
             />
         </View>
         <View className="h-3/5 w-screen z-1">
-          {/* <Floor/> */}
-          <SecondFloor/>
+        {(value == 'Floor 2') ?   <SecondFloor/> : <Floor/>}
         </View>
         <View className="h-2/6 w-full z-10">
             
 
         </View>
       </SafeAreaView>
-    </GestureHandlerRootView>
+    </GestureHandlerRootView>   
 
   );
 }
